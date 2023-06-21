9e470f891dc42db6d76fd96af65831c342bedfcc
Igor Maurell <igormaurell@gmail.com>
2023-06-21 10:33:07 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 FILE = Path(__file__).resolve()
 PARENT = FILE.parent  # root directory
 README = (PARENT / 'README.md').read_text(encoding='utf-8')
-REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
+#REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
 
 
 def get_version():
@@ -21,7 +21,7 @@ def get_version():
 setup(
     name='ultralytics',  # name of pypi package
     version=get_version(),  # version of pypi package
-    python_requires='>=3.7',
+    #python_requires='>=3.7',
     license='AGPL-3.0',
     description=('Ultralytics YOLOv8 for SOTA object detection, multi-object tracking, instance segmentation, '
                  'pose estimation and image classification.'),
@@ -36,7 +36,7 @@ setup(
     author_email='hello@ultralytics.com',
     packages=find_packages(),  # required
     include_package_data=True,
-    install_requires=REQUIREMENTS,
+    #install_requires=REQUIREMENTS,
     extras_require={
         'dev': [
             'check-manifest',
